f9790ffec2b164bcfe8913c3da435296b8a3a55d
Ashlin Kanawaty <kanawaa@mcmaster.ca>
2017-04-09 19:25:40 -0400

    Working version and report

--- pa4/main.c
+++ pa4/main.c
@@ -10,22 +10,19 @@ Prakhar Garg
 #include "ppmFile.h"
 #include <string.h>
 
-typedef struct {
-  unsigned char r,g,b;
-} RGB;
+int numProcesses; 
 
-void applyBlurFilter(Image* destImage, Image* srcImage, unsigned int height, unsigned int width, unsigned int windowSize);
-void setMean(unsigned char r[], unsigned char g[], unsigned char b[], int size, unsigned char* destImagePixel);
+void applyBlurFilter(int myRank, Image* destImage, Image* srcImage, unsigned long height, unsigned long width, unsigned long windowSize);
+void setMean(int myRank, Image* image, unsigned char r[], unsigned char g[], unsigned char b[], unsigned long size, unsigned long x, unsigned long y);
 
 int main(int argc, char** argv)
 {
   Image *image;           // original image, only the num of rows needed for each process
   Image *imageSlice;      // the processed image, only the num of rows needed for each process
   Image *testImage;       // the final complete image
-  unsigned int width;            // width of image
-  unsigned int height;           // height of image
-  int max = 255;        // max unsigned char value for a pixel channel in RGB   
-  int h0;               // height for process 0; h0 = rows[my_rank]
+  unsigned long width;            // width of image
+  unsigned long height;           // height of image
+  unsigned long h0;               // height for process 0; h0 = rows[my_rank]
 
   //filter is of size mxm
   //input.ppm output.ppm 3 A
@@ -33,42 +30,43 @@ int main(int argc, char** argv)
   char* infile, *outfile;
 
   //These are variables for console input
-  unsigned int radius;              // blur radius
+  unsigned long radius;              // blur radius
+  unsigned long filterSideLength = 0;
 
-  int my_rank;
-  int p;  
-  // p is number of processes; processes are numbered 0 to p-1; p is assigned when you call from the command line with
-  // mpirun -np p
-  int i, dest, source, offset;
+  int my_rank; 
+  // numProcesses is number of processes; processes are numbered 0 to numProcesses-1; numProcesses is assigned when you call from the command line with
+  // mpirun -np numProcesses
+  unsigned long i, dest, source, offset;
 
   MPI_Status status;
 
   infile = argv[2];
   outfile = argv[3];
-  radius = (unsigned int)atoi(argv[1]);
+  radius = (unsigned int)atol(argv[1]);
+  filterSideLength = 2* radius + 1;
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &p);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  //I may play around with this number later,
-  //I want the distribution of intervals to be gaussian
-  // int numIntervals = 100000;
-  // SearchInterval *intervals;
-  // intervals = (SearchInterval*)malloc(sizeof(SearchInterval)*numIntervals);
+  // MPI timer
+  // **** START TIMER **** //
+  double local_start, local_stop, local_elapsed, elapsed;
+  MPI_Barrier(MPI_COMM_WORLD);
+  local_start = MPI_Wtime();
 
-  int *rows;    // an array, each index tells each process how many rows it must process
-  rows = (int*)malloc(sizeof(int)*p);
+  unsigned long *rows;    // an array, each index tells each process how many rows it must process
+  rows = (unsigned long*)malloc(sizeof(unsigned long)*numProcesses);
 
   if(my_rank == 0)
   {
-    printf("infile: %s\n", infile);
-    printf("outfile: %s\n", outfile);
-    printf("radius: %u\n", radius);
+    printf("Infile: %s\n", infile);
+    printf("Outfile: %s\n", outfile);
+    printf("Radius: %lu\n", radius);
     image = ImageRead(infile);
     width = image->width;
     height = image->height;
-    printf("Image is %ux%u\n", width, height);
+    printf("Image is %lux%lu\n", width, height);
   }
 
   // need to send size of width and height
@@ -77,62 +75,67 @@ int main(int argc, char** argv)
 
   //allocating the number of rows to shoot over to a process
   //extra rows will wrap
-  for (i = 0 ;i < p ;i++)
-    rows[i] = height/p ;
-  for (i = 0; i < height % p ; i++)
+  for (i = 0 ;i < numProcesses ;i++)
+    rows[i] = height/numProcesses ;
+  for (i = 0; i < height % numProcesses ; i++)
     rows[i] ++;
   int tag = 0;
 
   // allocate memory for the image rows each process gets
   if(my_rank != 0)
   {
-    if(my_rank == p - 1)
-    {   // last process p-1
-      image = ImageCreate( width, (rows[my_rank] + (radius/2)) );
-      imageSlice = ImageCreate( width, (rows[my_rank] + (radius/2)) );
+    if(my_rank == numProcesses - 1)
+    {   // last process numProcesses-1
+      image = ImageCreate( width, (rows[my_rank] + (filterSideLength/2)) );
+      imageSlice = ImageCreate( width, (rows[my_rank] + (filterSideLength/2)) );
     }
     else
-    {   // processes 1 to p-2
-      image = ImageCreate( width, (rows[my_rank] + (radius-1)) );
-      imageSlice = ImageCreate( width, (rows[my_rank] + (radius-1)) );
+    {   // processes 1 to numProcesses-2
+      image = ImageCreate( width, (rows[my_rank] + (filterSideLength-1)) );
+      imageSlice = ImageCreate( width, (rows[my_rank] + (filterSideLength-1)) );
     }
   }
   else
   {   // process 0
-    if(p==1)
+    if(numProcesses==1)
     {   // if there is only one process
       h0 = rows[my_rank];
     }
     else
     {    // if there are multiple processes
-      h0 = rows[my_rank] + radius/2;
+      h0 = rows[my_rank] + filterSideLength/2;
     }
 
     testImage = ImageCreate( width, height );
     imageSlice = ImageCreate( width, h0 );
     memcpy( imageSlice->data, image->data, (h0 * width * sizeof(unsigned char) * 3) );
+    // printf("Reading P0: offset (in rows)=0, starting_row=0, size=%lu\n", h0 );
   }
 
   // send the original image to each process
   if(my_rank == 0)
   {
-      int offset = 0;
-      for(dest = 1; dest < p; dest++)
+      unsigned int offset = 0;
+      for(dest = 1; dest < numProcesses; dest++)
       {
         offset += rows[dest - 1];
         //buffer, count, filler...
         //The first and last process need the duplicate pixels from previous/last slice
-        if(dest == p - 1)
-        {   // last process, p-1
-          MPI_Send(&image->data[(offset - radius/2) * width],
-            (rows[dest] + (radius/2)) * width * sizeof(unsigned char) * 3,
+        if(dest == numProcesses - 1)
+        {   // last process, numProcesses-1
+          MPI_Send(&image->data[(offset - filterSideLength/2) * width * 3],
+            (rows[dest] + (filterSideLength/2)) * width * sizeof(unsigned char) * 3,
             MPI_BYTE, dest, tag, MPI_COMM_WORLD);
+          // printf("Reading P%lu: offset (in rows)=%u, starting_row=%lu, size=%lu\n", dest, offset, (offset - filterSideLength/2), 
+          //   (rows[dest] + (filterSideLength/2)) );
         }
         else
-        {     // other processes 1 to p-2
-          MPI_Send(&image->data[(offset - radius/2) * width],
-            (rows[dest] + (radius-1)) * width * sizeof(unsigned char) * 3,
+        {     // other processes 1 to numProcesses-2
+          MPI_Send(&image->data[(offset - filterSideLength/2) * width * 3],
+            (rows[dest] + (filterSideLength-1)) * width * sizeof(unsigned char) * 3,
             MPI_BYTE, dest, tag, MPI_COMM_WORLD);
+          // printf("Reading P%lu: offset (in rows)=%u, starting_row=%lu, size=%lu\n", dest, offset, (offset - filterSideLength/2), 
+          //   (rows[dest] + (filterSideLength-1)) );
         }
       }
   }
@@ -140,72 +143,83 @@ int main(int argc, char** argv)
   // receive original image
   else
   {
-    if(my_rank == p - 1)
+    if(my_rank == numProcesses - 1)
     {
       MPI_Recv(image->data,
-        (rows[my_rank] + (radius/2)) * width * sizeof(unsigned char) * 3,
+        (rows[my_rank] + (filterSideLength/2)) * width * sizeof(unsigned char) * 3,
         MPI_BYTE, 0, tag, MPI_COMM_WORLD, &status);
     }
     else
     {
       MPI_Recv(image->data,
-        (rows[my_rank] + (radius-1)) * width * sizeof(unsigned char)  * 3,
+        (rows[my_rank] + (filterSideLength-1)) * width * sizeof(unsigned char)  * 3,
         MPI_BYTE, 0, tag, MPI_COMM_WORLD, &status);
     }
   }
 
   // process the image
-  printf("Computing filter on process: %d\n", my_rank);
+  // printf("Computing filter on process: %d\n", my_rank);
   if(my_rank != 0)
   {
-    if(my_rank == p - 1)
+    if(my_rank == numProcesses - 1)
     {
-      applyBlurFilter(imageSlice, image, rows[my_rank] + radius/2, width, radius);
+      applyBlurFilter(my_rank, imageSlice, image, rows[my_rank] + filterSideLength/2, width, filterSideLength);
     }
     else
     {
-      applyBlurFilter(imageSlice, image, rows[my_rank] + (radius-1), width, radius);
+      applyBlurFilter(my_rank, imageSlice, image, rows[my_rank] + (filterSideLength-1), width, filterSideLength);
     }
   }
   else
   {
     // for process 0, imageSlice is just a copy of image
     //printf("processing %d  with length %lu from %d\n",h0 * width, rows[my_rank] * width * sizeof(RGB), 0);
-    applyBlurFilter(testImage, imageSlice, h0, width, radius);
+    applyBlurFilter(my_rank, testImage, imageSlice, h0, width, filterSideLength);
+    // printf("Writing P0: offset (in rows)=0, size=%lu\n", rows[0] );
   }
 
   // send the data back to process 0
   if(my_rank != 0)
   {
-        MPI_Send(&imageSlice->data[(radius/2) * width],
+        MPI_Send(&imageSlice->data[(filterSideLength/2) * width * 3],
           rows[my_rank] * width * sizeof(unsigned char) * 3,
           MPI_BYTE, 0, tag, MPI_COMM_WORLD);
-        printf("sending image\n" );
+        // printf("sending image\n" );
   }
   else
   {
     offset = 0;
-    for(source = 1; source < p; source++)
+    for(source = 1; source < numProcesses; source++)
     {
         offset += rows[source - 1];
-        MPI_Recv(&testImage->data[(offset) * width],
+        MPI_Recv(&testImage->data[offset * width * 3],
           rows[source] * width * sizeof(unsigned char) * 3,
           MPI_BYTE, source, tag, MPI_COMM_WORLD, &status);
+        // printf("Writing P%lu: offset (in rows)=%lu, size=%lu\n", source, offset, rows[source] );
     }
+    // printf("received image\n" );
   }
 
   // write the final image
   if(my_rank ==0)
   {
-    printf("Writing image...\n");
-    ImageWrite(testImage, outfile);
+    ImageWrite(testImage, outfile);   
     free(testImage);
   }
-  printf("Freeing memory!\n");
+  
 
-  free(imageSlice);
-  free(rows);
-  free(image);
+  // if (testImage != NULL) free(testImage);
+  if (imageSlice != NULL) free(imageSlice);
+  if (rows != NULL) free(rows);
+  if (image != NULL)free(image);
+
+  // **** STOP TIMER **** //
+  local_stop = MPI_Wtime();
+  local_elapsed = local_stop - local_start;
+  MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+  if (my_rank == 0)
+    printf("Elapsed time is %.2f seconds.\n", elapsed);
 
   MPI_Finalize();
 
@@ -214,59 +228,139 @@ int main(int argc, char** argv)
 
 //iterate over every pixel
 //for every pixel, take the window of pixels and average the R, G, B vals
-void applyBlurFilter(Image* destImage, Image* srcImage, unsigned int height, unsigned int width, unsigned int windowSize)
+void applyBlurFilter(int myRank, Image* destImage, Image* srcImage, unsigned long height, unsigned long width, unsigned long windowSize)
 {
-  unsigned int i,j= 0;
-  unsigned int row, col, size;
-  printf("width: %u\n", width);
-  printf("height: %u\n", height);
-  printf("window size: %u\n", windowSize);
+  unsigned long i,j= 0;
+  unsigned long row, col, size;
+  // printf("width: %lu\n", width);
+  // printf("height: %lu\n", height);
+  // printf("window size: %lu\n", windowSize);
 
-  for (i=0; i < height ;  i ++)
+  unsigned long radius = (windowSize - 1) / 2;
+  unsigned long startingRow, endingRow, startFilterRow, startFilterCol, endFilterRow, endFilterCol;
+
+  if (myRank == 0) 
   {
-    for(j=0; j  < width ; j ++)
+    startingRow = 0;
+    endingRow = height - radius;
+  }
+  else if (myRank == numProcesses - 1) 
+  {
+    startingRow = radius;
+    endingRow = height;
+  }
+  else 
+  {
+    startingRow = radius;
+    endingRow = height - radius;
+  }
+
+  for (i=startingRow; i < endingRow;  i ++)    // row
+  {
+    for(j=0; j  < width ; j ++)       // column
     {
       unsigned char red[windowSize*windowSize];
       unsigned char green[windowSize*windowSize];
       unsigned char blue[windowSize*windowSize];
-
+      
       size = 0;
-      for(row = (i - windowSize/2) * 3; row < (i + windowSize/2) * 3 ; row+=3)
+
+      // startFilterRow = i - windowSize/2;
+      // startFilterCol = j - windowSize/2;
+
+      if (windowSize/2 > i) startFilterRow = 0;
+      else startFilterRow = i - windowSize/2;
+      if (windowSize/2 > j) startFilterCol = 0;
+      else startFilterCol = j - windowSize/2;
+      if ((i + windowSize/2) > height) endFilterRow = height;
+      else endFilterRow = i + windowSize/2;
+      if ((j + windowSize/2) > width) endFilterCol = width;
+      else endFilterCol = j + windowSize/2;
+
+      for(row = startFilterRow; row < endFilterRow; row++)
       {
-        for(col = (j - windowSize/2) * 3; col < (j + windowSize/2) * 3; col+=3)
+        for(col = startFilterCol; col < endFilterCol; col++)
         {
-          // printf("i=%d, j=%d, size=%d, it=%d\n", i, j, size, row * width + col);
-          red[size] = srcImage->data[row * width + col];
-          green[size] = srcImage->data[row * width + col + 1];
-          blue[size] = srcImage->data[row * width + col + 2];
-          size+=1;
+          red[size] = ImageGetPixel(srcImage, col, row, 0);
+          green[size] = ImageGetPixel(srcImage, col, row, 1);
+          blue[size] = ImageGetPixel(srcImage, col, row, 2);
+          size++;       
         }
       }
-      
-      setMean(red, green, blue, size, &destImage->data[i*width + j]);    
 
+      setMean(myRank, destImage, red, green, blue, size, j, i);          
     }
   }
-  printf("Completed blurring.\n");
+  // printf("Completed blurring.\n");
 }
 
-void setMean(unsigned char r[], unsigned char g[], unsigned char b[], int size, unsigned char* destImagePixel)
+void setMean(int myRank, Image* destImage, unsigned char r[], unsigned char g[], unsigned char b[], unsigned long size, unsigned long x, unsigned long y)
 {
-  unsigned char sumR = 0;
-  unsigned char sumG = 0;
-  unsigned char sumB = 0;
+  unsigned long sumR = 0;
+  unsigned long sumG = 0;
+  unsigned long sumB = 0;
+  unsigned char avgR = 0;
+  unsigned char avgG = 0;
+  unsigned char avgB = 0;
+
   int j;
   for(j = 0; j < size; j++)
   {
      sumR += r[j];
      sumG += g[j];
      sumB += b[j];
-  }
+  }  
 
   if(size !=0)
   {
-    *destImagePixel = sumR / size;
-    *(destImagePixel + 1) = sumG / size;
-    *(destImagePixel + 2) = sumB / size;
+    if (sumR/size > 255)
+    {
+      avgR = 255;
+    }
+    else
+    {
+      avgR = (unsigned char) (sumR / size);
+    }
+    if (sumR/size > 255)
+    {
+      avgG = 255;
+    }
+    else
+    {
+      avgG = (unsigned char) (sumG / size);
+    }
+    if (sumR/size > 255)
+    {
+      avgB = 255;
+    }
+    else
+    {
+      avgB = (unsigned char) (sumB / size);
+    }
+
+    // if (myRank == 1)
+    // {
+    //   avgR = 255;
+    //   avgG = 0;
+    //   avgB = 0;
+    // }
+    // else if (myRank == 2)
+    // {
+    //   avgR = 0;
+    //   avgG = 255;
+    //   avgB = 0;
+    // }
+    // else if (myRank == 3)
+    // {
+    //   avgR = 0;
+    //   avgG = 0;
+    //   avgB = 255;
+    // }
+
+    ImageSetPixel(destImage, x, y, 0, avgR);
+    ImageSetPixel(destImage, x, y, 1, avgG);
+    ImageSetPixel(destImage, x, y, 2, avgB);
+
   }
+  // else printf("size is 0 at x,y = %u,%u\n", x, y);
 }
